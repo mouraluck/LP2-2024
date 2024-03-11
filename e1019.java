c14addaddf5e48149436ade7933b6aeea9e18c7d
O Moura <123185795+LucRevoltz207@users.noreply.github.com>
2024-03-10 23:21:22 -0300

    Exercícios Beecrowd

--- e1019.java
+++ e1019.java
@@ -6,7 +6,11 @@ public class e1019 {
     public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
         int sec = s.nextInt();
-        int h = sec/60;
+        int h = sec%3600;
+        System.out.print(sec/3600+":");
+        System.out.print(h/60+":");
+        int sec_sobra = h % 60;
+        System.out.print(sec_sobra+"\n");
         
     }
 }
